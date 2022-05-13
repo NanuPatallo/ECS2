84daa5fb69b21e9b47e095ad8634c91671f767af
NanuPatallo <daianapatallo@gmail.com>
2022-05-12 23:16:40 -0300

    Dni y Email

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ Crear un repositorio en GitHub llamado ECS2 ok
 Clonar el repositorio dentro del root del servidor PHP. ok
 Agregar un archivo index.php ok
 Resolver el siguiente enunciado dentro del archivo index.php
-Recibir dos variables mediante post.
+Recibir dos variables mediante post. ok
 Dni (obligatoria, si no se envía devolver el mensaje. “DNI obligatorio.”) 
 Email.
 Devolver:
@@ -14,10 +14,12 @@ Realizar el commit de los cambios, con el mensaje “Dni y email”
 Pushear los cambios al repositorio remoto.
 Para entregar la evaluación copiar el link del repositorio.*/
 
-isset ($_POST['DNI']);
-
-isset ($_POST['E-mail']);
-
-
-
-
+if (isset($_POST['DNI']) == false) {
+    echo 'DNI => ¡Obligatorio!';
+} else {
+    echo 'DNI: ' . $_POST['DNI'];
+    echo '<hr>';
+    if (isset($_POST['E-mail'])) {
+        echo 'E-mail: ' . $_POST['E-mail'];
+    }
+}
