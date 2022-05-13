e8f2e7341b7b944b552a3ccffdd7f5a4d7e8aad1
NanuPatallo <daianapatallo@gmail.com>
2022-05-12 22:37:24 -0300

    1er Commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/*Evaluación conceptual de situación N2 
+Crear un repositorio en GitHub llamado ECS2 ok
+Clonar el repositorio dentro del root del servidor PHP. ok
+Agregar un archivo index.php ok
+Resolver el siguiente enunciado dentro del archivo index.php
+Recibir dos variables mediante post.
+Dni (obligatoria, si no se envía devolver el mensaje. “DNI obligatorio.”) 
+Email.
+Devolver:
+Dni: [valorDNI]
+Email: [valorEmail] (En caso de no enviar el mail, no mostrar nada, tampoco el label Email)
+Realizar el commit de los cambios, con el mensaje “Dni y email”
+Pushear los cambios al repositorio remoto.
+Para entregar la evaluación copiar el link del repositorio.*/
+
+isset ($_POST['DNI']);
+
+isset ($_POST['E-mail']);
+
+
+
+
